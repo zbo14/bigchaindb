55a11c6f376cbb327f558eb3a9edef90233e3f65
Rodolphe Marques <rodolphe@ascribe.io>
2016-05-25 09:48:10 +0200

    more speed tests

--- speed-tests/speed_tests.py
+++ speed-tests/speed_tests.py
@@ -1,5 +1,6 @@
 import time
 import json
+import multiprocessing as mp
 
 import rapidjson
 from line_profiler import LineProfiler
@@ -85,6 +86,122 @@ def speedtest_deserialize_block_rapidjson():
     time_elapsed = time.time() - time_start
 
     print('speedtest_deserialize_block_rapidjson: {} s'.format(time_elapsed))
+
+
+def _queue_writer_worker(queue, tx, n):
+    for _ in range(n):
+        queue.put(tx)
+    queue.cancel_join_thread()
+
+
+def _queue_reader_worker(queue, tx, n):
+    for i in range(n):
+        print('reading from queue', i)
+        queue.get(tx)
+    queue.cancel_join_thread()
+
+
+
+def speedtest_write_to_queue_single_core():
+    # create a transaction
+    b = bigchaindb.Bigchain()
+    tx = b.create_transaction(b.me, b.me, None, 'CREATE')
+    tx_signed = b.sign_transaction(tx, b.me_private)
+
+    # create queue
+    queue = mp.Queue()
+
+    time_start = time.time()
+    for _ in range(1000):
+        queue.put(tx_signed)
+    time_elapsed = time.time() - time_start
+
+    print('speedtest_write_to_queue_single_core: {} s'.format(time_elapsed))
+
+
+def speedtest_write_to_queue_multicore():
+    # create a transaction
+    b = bigchaindb.Bigchain()
+    tx = b.create_transaction(b.me, b.me, None, 'CREATE')
+    tx_signed = b.sign_transaction(tx, b.me_private)
+    num_transactions = 1000000
+
+    # create queue
+    queue = mp.Queue()
+
+    # create the processes
+    processes = []
+    for _ in range(mp.cpu_count()):
+        processes.append(mp.Process(target=_queue_writer_worker,
+                                    args=(queue, tx_signed, num_transactions // mp.cpu_count())))
+
+    time_start = time.time()
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+    time_elapsed = time.time() - time_start
+
+    print('speedtest_write_to_queue_multicore: {} s'.format(time_elapsed))
+
+
+def speedtest_read_queue_single_core():
+    # create a transaction
+    b = bigchaindb.Bigchain()
+    tx = b.create_transaction(b.me, b.me, None, 'CREATE')
+    tx_signed = b.sign_transaction(tx, b.me_private)
+
+    # create queue
+    queue = mp.Queue()
+    for _ in range(1000):
+        queue.put(tx_signed)
+
+    time_start = time.time()
+    for _ in range(1000):
+        queue.get()
+    time_elapsed = time.time() - time_start
+
+    print('speedtest_read_queue_single_core: {} s'.format(time_elapsed))
+
+
+def speedtest_read_queue_multicore():
+    # create a transaction
+    b = bigchaindb.Bigchain()
+    tx = b.create_transaction(b.me, b.me, None, 'CREATE')
+    tx_signed = b.sign_transaction(tx, b.me_private)
+    num_transactions = 10000
+
+    # create queue
+    queue = mp.Queue()
+    processes = []
+    for _ in range(mp.cpu_count()):
+        processes.append(mp.Process(target=_queue_writer_worker,
+                                    args=(queue, tx_signed, num_transactions // mp.cpu_count())))
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+
+    print('finished filling queue')
+
+    # create the processes
+    processes = []
+    for _ in range(mp.cpu_count()):
+        processes.append(mp.Process(target=_queue_reader_worker,
+                                    args=(queue, tx_signed, num_transactions // mp.cpu_count())))
+
+    time_start = time.time()
+    for p in processes:
+        p.start()
+    print('started reading transactions')
+    for p in processes:
+        p.join()
+
+    print('finished reading transactions')
+    time_elapsed = time.time() - time_start
+
+    print('speedtest_read_queue_multicore: {} s'.format(time_elapsed))
 
 
 if __name__ == '__main__':
