0127f571f2d48d0cdc6122d092312d19756f5815
Scott Sadler <scott@alphanode.co>
2016-12-06 15:41:42 +0100

    show changes to inputs and outputs tx

--- test_inputs_ouptuts_spec.py
+++ test_inputs_ouptuts_spec.py
@@ -8,24 +8,20 @@ def test_inputs_outputs_spec():
         "asset": {
             "id": "99a15f79-850d-45d6-9abb-91741cd0bc08"
         },
-        "fulfillments": [
+        "inputs": [
             {
-                "fid": 0,
-                "input": {
-                    "cid": 0,
-                    "txid": "346cd85088c30ac5e0b6beda000adb15e53a74b0f974ce98711e2c2c092093f2"
+                "spends": {
+                    "idx": 0,
+                    "txid": "0fd6ae9744f88303f7a473e8f3de1027fb1130af49c7bc85730cdc557abb4415"
                 },
-                "fulfillment": "cf:4:__Y_Um6H73iwPe6ejWXEw930SQhqVGjtAHTXilPp0P3D7mHJpq4bGv8WwQu6bgl4m3Kjy0Zagi30RR-K6QzWUyWtO-ak3I69EdgdqygaAIEcJAVNrCWWDA_aamC7kZ8B",
-                "owners_before": [
-                    "JEAkEJqLbbgDRAtMm8YAjGp759Aq2qTn9eaEHUj2XePE"
-                ]
+                "unlocking_script": "cf:4:__Y_Um6H73iwPe6ejWXEw930SQhqVGjtAHTXilPp0P1KyUuqP0PrHPi37sSPKVtI0xN2SjSWBHFxVn-2qTDQrgFYSHSCbALBJcA5dwvSxut49TA7koXx0bhWdxMjvhkO",
             }
         ],
-        "conditions": [
+        "outputs": [
             {
                 "amount": 1,
-                "cid": 0,
-                "condition": {
+                "language": "cc",
+                "locking_script": {
                     "details": {
                         "bitmask": 32,
                         "type_id": 4,
@@ -35,7 +31,7 @@ def test_inputs_outputs_spec():
                     },
                     "uri": "cc:4:20:__Y_Um6H73iwPe6ejWXEw930SQhqVGjtAHTXilPp0P0:96"
                 },
-                "owners_after": [
+                "public_keys": [
                     "JEAkEJqLbbgDRAtMm8YAjGp759Aq2qTn9eaEHUj2XePE"
                 ]
             }
