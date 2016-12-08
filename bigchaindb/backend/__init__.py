78ba5ed23f74bc3d00e9bd0ed9dfda214407b2db
Rodolphe Marques <marques.rodolphe@gmail.com>
2016-12-08 18:16:16 +0100

    Initial implementation of the changefeed abstraction

--- bigchaindb/backend/__init__.py
+++ bigchaindb/backend/__init__.py
@@ -4,3 +4,4 @@
 from bigchaindb.backend import changefeed, schema, query  # noqa
 
 from bigchaindb.backend.connection import connect  # noqa
+from bigchaindb.backend.changefeed import get_changefeed  # noqa
