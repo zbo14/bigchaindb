6c78417863ab0ad8a6dab5b7b5ceafa3942023df
Scott Sadler <scott@alphanode.co>
2017-02-22 15:42:47 +0100

    try to reproduce #1208

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -79,3 +79,37 @@ def test_get_owned_ids_works_after_double_spend(b, user_pk, user_sk):
     b.get_owned_ids(user_pk)
     with pytest.raises(DoubleSpend):
         b.validate_transaction(tx_double_spend)
+
+
+def test_hyperchain_double_inclusion(b):
+    from bigchaindb.models import Transaction
+    from bigchaindb.common.crypto import generate_key_pair
+
+    dimi = generate_key_pair()
+    mark = generate_key_pair()
+
+    def wait_tx(txid):
+        while True:
+            _, status = b.get_transaction(txid, include_status=True)
+            if status in ('valid', 'invalid'):
+                break
+            time.sleep(0.1)
+
+    for i in [0, 1]:
+        tx = Transaction.create([dimi[1]], [([dimi[1]], 1)]).sign([dimi[0]])
+
+        b.write_transaction(tx)
+        wait_tx(tx.id)
+
+        spend = Transaction.transfer(tx.to_inputs(), [([mark[1]], 1)],
+                                     tx.id).sign([dimi[0]])
+
+        b.write_transaction(spend)
+        wait_tx(spend.id)
+
+        b.write_transaction(spend)
+        wait_tx(spend.id)
+
+        assert [own.to_dict() for own in b.get_owned_ids(dimi[1])] == []
+        assert [own.to_dict() for own in b.get_owned_ids(mark[1])] == \
+               [{'txid': spend.id, 'output': 0}]
